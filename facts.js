53197bf275a6df50b743a5e6dc69b25d7f721e83
TheGreekBrit <howdy140@gmail.com>
2015-12-23 09:47:19 -0500

    init commit

---
+++ facts.js
@@ -0,0 +1,18 @@
+The kakapo is the heaviest parrot in the world.
+The kakapo is possibly the longest-living bird species in the world. The low adult mortality rate suggests and average life expectancy of 90 years!
+The kakapo is large and flightless bird.
+In the breeding season, the male kakapo can inflate like a balloon and emit a low ‘sonic’ boom which, in mountainous terrain, can be heard up to five kilometres away.
+The traditional diet of kakapo is strictly herbivorous.
+Kakapo have thighs typical of New Zealand’s more famous All Black rugby forwards, allowing them to walk several kilometers at a time, and to turn on a fair burst of speed as well.
+The kakapo is the heavyweight champion of the parrot world; smaller females typically weighing 1.4kg, and males 2.2kg. Fat reserves of a kilogram or more can be added prior to a breeding season.
+It is thought that, many hundreds of thousands of years ago, the kakapo was probably a typical, lightweight parrot that flew through the air to travel and gather its food. But the process of evolution without the presence and effect of mammals in New Zealand saw the bird give away its flying skills, put on weight, and become a good hiker and climber instead, with powerful claws.
+The kakapo is best described as a midnight rambler, spending most of its life sleeping during the day and wandering alone through the forest at night.
+Kakapo have a fucking scary-ass call, called a "skraark". It's awful. Check it out: http://kakaporecovery.org.nz/wp-content/uploads/2013/03/skraark.mp3
+Kakapo are solitary creatures. They gather only to breed, and the females are left to raise their chicks alone. Typically, each bird keeps to itself during the day, tucked up in a roost on the ground or in a tree, and venturing out at night to feed.
+A peculiar habit of the kakapo is to freeze when disturbed, keeping absolutely still and hoping to blend into the background. Most animals have evolved more useful forms of defence, such as taking flight; but the kakapo obviously did not need this behaviour in the days when New Zealand’s only predators were avian, such as Haast’s eagle.
+Everything in the kakapo’s lifestyle happens slowly. Males do not start breeding until they are about four years old, while females do not start until they are at least six years old. Even then, breeding does not take place every year. Rather, it seems to be dependent on the availability of key food supplies, such as the fruiting of rimu trees, and will happen on average only every two to four years.
+None of the kakapo whose ages we do know have yet died of old age, even though most were discovered 15-30 years ago. In fact, it may not be known how long kakapo live until some of the birds hatched during the Kakapo Recovery program die of old age, which might be several decades away.
+Kakapo freeze when startled. One of their defenses is to freeze and hope to blend into the background when danger is near.
+Kakapo are friendly! Both the Maori and early European settlers kept kakapo as pets. Even wild kakapo are known to approach, climb on, and preen people.
+Kakapo are critically endangered. The kakapo problems began with Maori settlers, and intensified when Europeans arrived. Both groups cleared large areas of the kakapo habitat and brought with them predators like cats, rats, and stoats that the kakapo had no defenses against.
+Watch a kakapo named Sirocco attempt to mate with some dude's head while Stephen Fry laughs at him: https://www.youtube.com/watch?v=9T1vfsHYiKY
